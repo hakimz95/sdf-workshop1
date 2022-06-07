a6a20b1672559e214fefc8f2a87377fcca8e85cc
hakimz95 <abdhakim119@gmail.com>
2022-06-07 12:08:15 +0800

    update

---
+++ cart/DataStructure.java
@@ -0,0 +1,23 @@
+package cart;
+
+import java.util.*;
+
+public class DataStructure {
+
+    public static void main(String[] args) {
+        System.out.println("Data STructure");
+        List<Integer> intList = new LinkedList<Integer>();
+
+        intList.add(Integer.parseInt("12"));
+        intList.add(10);
+        intList.add(20);
+        intList.add(0, 56);
+
+        System.out.println(intList);
+
+        for(int i=0; i< intList.size(); i++) {
+            System.out.printf("idx: %d \n",i);
+            System.out.printf("val: %d \n",intList.get(i));
+        }
+    }
+}
---
+++ cart/ShoppingCart.java
@@ -0,0 +1,8 @@
+package cart;
+
+public class ShoppingCart {
+    public static void main(String[] args) {
+        System.out.println("Shopping Cart");
+        List<String>
+    }
+}
